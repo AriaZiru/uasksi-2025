b1877a33312c0d2a68b1dbeb0db755497f9a67e6
AriaZiru <aryadildiangka24@gmail.com>
2025-07-23 06:57:06 +0700

    chore(db): setup db peminjaman barang

--- src/database/migrations/2025_07_22_220219_create_peminjaman_barangs_table.php
+++ src/database/migrations/2025_07_22_220219_create_peminjaman_barangs_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('peminjaman_barangs', function (Blueprint $table) {
             $table->id();
+            $table->string('nama');
+            $table->string('nomor_telepon');
+            $table->string('email')->nullable();
+            $table->date('tanggal_pinjam');
+            $table->date('tanggal_kembali')->nullable();
             $table->timestamps();
         });
     }
