cbe98203971bf0b0dae6046ccff33d329e810683
AriaZiru <aryadildiangka24@gmail.com>
2025-07-23 15:07:11 +0700

    validate daftar barang

--- src/app/Http/Controllers/DaftarBarangController.php
+++ src/app/Http/Controllers/DaftarBarangController.php
@@ -6,5 +6,17 @@ use Illuminate\Http\Request;
 
 class DaftarBarangController extends Controller
 {
-    //
+    public function store(Request $request)
+{
+    $validated = $request->validate([
+        'nama_barang' => 'required|string|max:255',
+        'kondisi_barang' => 'required|in:baik,rusak,servis',
+        'status' => 'required|in:aktif,dipinjam,hilang,rusak',
+        'jumlah' => 'required|integer|min:0',
+    ]);
+
+    DaftarBarang::create($validated);
+
+    return response()->json(['message' => 'Barang berhasil ditambahkan']);
+}
 }
