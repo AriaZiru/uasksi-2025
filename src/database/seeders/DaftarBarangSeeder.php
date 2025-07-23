c4431600b81f3707f9a8df22cb673dace7fcbc47
AriaZiru <aryadildiangka24@gmail.com>
2025-07-23 10:12:15 +0700

    feat(seeder): menambahkan seeder untuk data barang inventaris kantor

--- src/database/seeders/DaftarBarangSeeder.php
+++ src/database/seeders/DaftarBarangSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\DaftarBarang;
 
 class DaftarBarangSeeder extends Seeder
 {
@@ -12,6 +13,41 @@ class DaftarBarangSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $data = [
+            [
+                'nama_barang' => 'Laptop ASUS ROG',
+                'kondisi_barang' => 'baik',
+                'status' => 'aktif',
+                'jumlah' => 5,
+            ],
+            [
+                'nama_barang' => 'Printer Epson L3110',
+                'kondisi_barang' => 'baik',
+                'status' => 'aktif',
+                'jumlah' => 2,
+            ],
+            [
+                'nama_barang' => 'Kursi Kantor Ergonomis',
+                'kondisi_barang' => 'baik',
+                'status' => 'aktif',
+                'jumlah' => 10,
+            ],
+            [
+                'nama_barang' => 'Meja Kerja Kayu',
+                'kondisi_barang' => 'baik',
+                'status' => 'aktif',
+                'jumlah' => 3,
+            ],
+            [
+                'nama_barang' => 'Proyektor BenQ',
+                'kondisi_barang' => 'baik',
+                'status' => 'hilang',
+                'jumlah' => 1,
+            ],
+        ];
+
+        foreach ($data as $barang) {
+            DaftarBarang::create($barang);
+        }
     }
 }
